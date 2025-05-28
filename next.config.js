c2804f6ad3b91b9ec8d2c058a8fc9adf69dea729
M7Joshi <Joshimanav0717@gmail.com>
2025-05-27 23:12:49 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true
-  },
   basePath: '/Manav_Portfolio.github.io',
-  assetPrefix: '/Manav_Portfolio.github.io/'
+  assetPrefix: '/Manav_Portfolio.github.io/',
 };
 
 module.exports = nextConfig;
